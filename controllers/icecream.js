fbf1dd2c408f017488fb1c5628d675e00ca7a841
Puru Huthika Yerragunta <S560279@nwmissouri.edu>
2023-04-13 20:15:23 -0500

    updated

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -1,21 +1,67 @@
 var icecream = require('../models/icecream');
 // List of all icecreams
-exports.icecream_list = function(req, res) {
-res.send('NOT IMPLEMENTED: icecream list');
-};
+exports.icecream_list = async function(req, res) {
+//es.send('NOT IMPLEMENTED: icecream list');
+//exports.icecream_list = async function(req, res) {
+    try{
+    theicecreams = await icecream.find();
+    res.send(theicecreams);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
 // for a specific icecream.
-exports.icecream_detail = function(req, res) {
+exports.icecream_detail = async function(req, res) {
 res.send('NOT IMPLEMENTED: icecream detail: ' + req.params.id);
-};
+
+    };
 // Handle icecream create on POST.
-exports.icecream_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: icecream create POST');
-};
+exports.icecream_create_post = async function(req, res) {
+//res.send('NOT IMPLEMENTED: icecream create POST');
+//exports.icecream_list = async function(req, res) {
+    console.log(req.body)
+    let document = new icecream();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.icecream_flavour = req.body.icecream_flavour;
+    document.icecream_size = req.body.icecream_size;
+    document.icecream_cost = req.body.icecream_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // Handle icecream delete form on DELETE.
-exports.icecream_delete = function(req, res) {
+exports.icecream_delete = async function(req, res) {
 res.send('NOT IMPLEMENTED: icecream delete DELETE ' + req.params.id);
 };
+
+
 // Handle icecream update form on PUT.
-exports.icecream_update_put = function(req, res) {
+exports.icecream_update_put = async function(req, res) {
 res.send('NOT IMPLEMENTED: icecream update PUT' + req.params.id);
-};
+
+    };
+// VIEWS
+// Handle a show all view
+exports.icecream_view_all_Page = async function(req, res) {
+    try{
+    theicecreams = await icecream.find();
+    res.render('icecream', { title: 'icecream Search Results', results: theicecreams });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+    
--- routes/icecream.js
+++ routes/icecream.js
@@ -1,11 +1,7 @@
 var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('icecream', { title: 'Search Results icecream' });
-});
-
-module.exports = router;
-
+    const icecream_controlers= require('../controllers/icecream');
+    var router = express.Router();
+    /* GET costumes */
+    router.get('/', icecream_controlers.icecream_view_all_Page );
+    module.exports = router;
 
