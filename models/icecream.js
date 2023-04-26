e873fb5c8b6fe5125002f53d1d13ffda9c8ea5d1
Puru Huthika Yerragunta <S560279@nwmissouri.edu>
2023-04-26 17:23:26 -0500

    Done with updates

--- models/icecream.js
+++ models/icecream.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose")
 const icecreamSchema = mongoose.Schema({
 icecream_flavour: String,
-icecream_size: String,
-icecream_cost: Number
+icecream_size: {
+    type: String,
+    required: true,
+    match: /^[a-zA-Z]+$/
+  },
+icecream_cost: {
+    type:Number,
+    min: 1,
+    max: 7000 } 
 })
 module.exports = mongoose.model("Icecream",
 icecreamSchema)
--- routes/icecream.js
+++ routes/icecream.js
@@ -1,15 +1,30 @@
 var express = require('express');
     const icecream_controlers= require('../controllers/icecream');
     var router = express.Router();
-    /* GET costumes */
-    router.get('/', icecream_controlers.icecream_view_all_Page );
 
-    /* GET detail costume page */
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+
+    
+/* GET costumes */
+router.get('/', icecream_controlers.icecream_view_all_Page );
+
+/* GET detail costume page */
 router.get('/detail', icecream_controlers.icecream_view_one_Page);
-    module.exports = router;
+
 /* GET create costume page */
-router.get('/create', icecream_controlers.icecream_create_Page);
+router.get('/create',secured, icecream_controlers.icecream_create_Page);
 /* GET create update page */
-router.get('/update', icecream_controlers.icecream_update_Page);
+router.get('/update',secured, icecream_controlers.icecream_update_Page);
 /* GET delete costume page */
-router.get('/delete', icecream_controlers.icecream_delete_Page);
+router.get('/delete',secured, icecream_controlers.icecream_delete_Page);
+module.exports = router;
